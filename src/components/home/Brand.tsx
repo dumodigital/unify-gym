07fbadb2362f99631c30ccd2af192f35d0d289ee
“Charlie <info@dumodigital.com>
2025-08-22 18:39:30 -0400

    style(brand): replace straight line with swoosh underline at same position

--- src/components/home/Brand.tsx
+++ src/components/home/Brand.tsx
@@ -47,7 +47,13 @@ export default function Brand() {
           <h2 className="relative z-10 font-display text-5xl md:text-6xl lg:text-7xl tracking-wide2">
             {renderStaggeredText('UNIFY FITNESS')}
           </h2>
-          <div className="absolute -bottom-2 left-[10%] w-4/5 h-1 bg-primary opacity-80"></div>
+          <div className="absolute -bottom-2 left-[10%] w-4/5 h-1">
+            <img
+              src="/brand-underline.svg"
+              alt=""
+              className="w-full h-full object-contain opacity-80"
+            />
+          </div>
         </div>
 
         {/* Copy with fade-in from left */}
