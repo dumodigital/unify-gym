d001b77a1945e63d528379280cb39de5ef393712
“Charlie <info@dumodigital.com>
2025-08-22 18:18:27 -0400

    fix(brand): make swoosh underline much smaller and thinner under UNIFY FITNESS header

--- src/components/home/Brand.tsx
+++ src/components/home/Brand.tsx
@@ -58,11 +58,11 @@ export default function Brand() {
           <h2 className="relative z-10 font-display text-5xl md:text-6xl lg:text-7xl tracking-wide2">
             {renderStaggeredText('UNIFY FITNESS')}
           </h2>
-          <div className="absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-full max-w-[80%] h-8 md:h-12">
+          <div className="absolute -bottom-1 left-1/2 transform -translate-x-1/2 w-[60%] h-3">
             <img
               src="/brand-underline.svg"
               alt=""
-              className="w-full h-full object-contain opacity-80"
+              className="w-full h-full object-contain opacity-70"
             />
           </div>
         </div>
